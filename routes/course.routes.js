f42eb0995ec93280cd3b59125eb843188a6790bf
kareem7199 <kareemtameregy@gmail.com>
2024-05-01 15:00:58 +0300

    feat : added course controller

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,153 +1,16 @@
 import express from"express"
-import {ACTIVITIES , TEACHERS , COURSES , COURSE_STUDENT} from"../models/index.models.js";
-import ApiErrorResponse from'../helpers/ApiErrorResponse.js'
-import ApiResponse from"../helpers/ApiResponse.js"
 import { verifyAdmin, verifyUser, verifyTeacher } from"../middlewares/verifyToken.js"
+import * as courseController from "../controllers/course.controller.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-    try {
-        const cousrses = await COURSES.findAll({
-            attributes : ["id", "name" , "price"],
-            raw : true,
-            include : {
-                attributes: ["name"],
-                model: TEACHERS
-            }
-        });
-        res.send(ApiResponse.success(cousrses));
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(ApiErrorResponse.InternalServerError());
-    }
-})
-
-router.get("/:id", async (req, res) => {
-    try {
-
-        const course = await COURSES.findOne({
-            raw: true,
-            where: {
-                id: req.params.id
-            },
-            include: {
-                model: TEACHERS,
-                attributes: ["name", "profilePicture"]
-            }
-        });
-
-
-        if (!course)
-            return res.status(404).send(ApiErrorResponse.NotFound());
-
-        res.send(ApiResponse.success(course));
-
-    } catch (error) {
-        res.status(500).send(ApiErrorResponse.InternalServerError(500));
-    }
-})
-
-router.post("/activity/:id", verifyTeacher, async (req, res) => {
-    try {
-
-        const teacherId = req.teacher.id;
-        const coursestudentId = req.params.id;
-        const { grade, title, fullMark } = req.body;
-
-        const courseStudent = await COURSE_STUDENT.findByPk(coursestudentId);
-
-        if (!courseStudent.isPaid)
-            return res.status(400).send(ApiErrorResponse.BadRequest());
-
-        const course = await COURSES.findOne({
-            where: {
-                teacherId: teacherId
-            }
-        })
-
-        if (!course)
-            return res.status(400).send(ApiErrorResponse.BadRequest());
-
-        const newActivity = await ACTIVITIES.create({
-            title,
-            grade,
-            fullMark,
-            coursestudentId: coursestudentId
-        })
-
-        res.send(ApiResponse.created(newActivity));
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(ApiErrorResponse.InternalServerError());
-    }
-})
-
-router.post("/enroll", verifyUser, async (req, res) => {
-    try {
-
-        const newCourseStudent = await COURSE_STUDENT.create({
-            courseId: req.body.courseId,
-            userId: req.user.id
-        });
-
-        res.send(ApiResponse.created(newCourseStudent));
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(ApiErrorResponse.InternalServerError());
-    }
-})
-
-router.post("/", verifyAdmin, async (req, res) => {
-    try {
-
-        const newCourse = await COURSES.create(req.body);
-
-        if (!newCourse)
-            return res.status(404).send(ApiErrorResponse.BadRequest());
-
-        res.send(ApiResponse.created(newCourse));
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(ApiErrorResponse.InternalServerError());
-    }
-})
-
-router.put("/:id", verifyAdmin, async (req, res) => {
-    try {
-
-        const course = await COURSES.findByPk(req.params.id);
-
-        if (!course)
-            return res.status(404).send(ApiErrorResponse.NotFound());
-
-        const updatedCourse = await course.update(req.body);
-
-        res.send(ApiResponse.success(updatedCourse, "Course updated successfully"));
-
-    } catch (error) {
-        res.status(500).send(ApiErrorResponse.InternalServerError());
-    }
-})
-
-router.delete("/:id", verifyAdmin, async (req, res) => {
-    try {
-
-        const course = await COURSES.findByPk(req.params.id);
-
-        if (!course)
-            return res.status(404).send(ApiErrorResponse.NotFound());
-
-        await course.destroy();
-
-        res.send(ApiResponse.success(null, "Course deleted successfully"));
-
-    } catch (error) {
-        res.status(500).send(ApiErrorResponse.InternalServerError());
-    }
-})
+router
+    .get("/", courseController.getCourses)
+    .get("/:id", courseController.getCourse)
+    .post("/", verifyAdmin, courseController.createCourse)
+    .post("/enroll", verifyUser, courseController.enroll)
+    .post("/activity/:id", verifyTeacher, courseController.addActivityToStudent)
+    .put("/:id", verifyAdmin, courseController.updateCourse)
+    .delete("/:id", verifyAdmin, courseController.deleteCourseById)
 
 export default router;
